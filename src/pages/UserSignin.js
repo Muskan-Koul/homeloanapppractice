d3135a570780207319e5a3145df4400385e64e56
Muskan-Koul <koulmuskan1998@gmail.com>
2021-07-14 00:42:47 +0530

    dropdown added

--- src/pages/UserSignin.js
+++ src/pages/UserSignin.js
@@ -117,6 +117,8 @@ export const UserSignin = () => {
             //dispatch the call to the redux: for the api call
             //todo
             //on success, it will redirect to the next page
+
+            // dispatch(authenticateUserAction({ userName, password }));
         }
         else{
             e.stopPropagation();
@@ -127,7 +129,7 @@ export const UserSignin = () => {
     return (
 
         <div className="bg-dark d-flex justify-content-center align-items-center" style={{height: "100vh"}}>
-            <div className="w-75">
+            <div className="w-50">
         <h1 className="text-center alert alert-info">User SignIn </h1>
         <form ref={formEl} className="needs-validation" noValidate>
             <div>
@@ -144,13 +146,16 @@ export const UserSignin = () => {
                 required
                  />
             </div>
-            <div>
-                <input type="dropdown" 
-                placeholder="Select Role" 
-                className="form-control form-control-lg mb-2"
-                required
-                 />
-            </div>
+           <div>
+              <select className="form-control form-control-lg mb-2" required >      
+              <option  placeholder="Select Role">Select Role</option>
+                <option value="admin">Admin</option>
+                <option value="customer">Customer</option>
+                <option value="landofficer">Land Officer</option>
+                <option value="Financeofficer">Finance Officer</option>
+              </select>
+              </div>
+
               <div>  
                 <input type="button"
                  value="Sign In"
